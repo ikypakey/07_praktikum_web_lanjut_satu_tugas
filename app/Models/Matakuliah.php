64d34bf9e73235370e36b99306a13bf48499233b
masbarok193@gmail.com <masbarok193@gmail.com>
2022-04-10 00:05:11 +0700

    edit on matakuliah models

--- app/Models/Matakuliah.php
+++ app/Models/Matakuliah.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasiswa_Matakuliah;
 
 class Matakuliah extends Model
 {
     use HasFactory;
+    protected $table = 'matakuliah';
+    protected $guarded = ['id'];
+
+    public function mahasiswa()
+    {
+        return $this->belongsToMany(Mahasiwa::class, 'mahasiswa_matakuliah', 'matakuliah_id', 'mahasiswa_id')->withPivot('nilai');
+    }
+    
 }
